816694ac81399ad00833b64aa8e04527f74d992a
Paz <robertparrish@monzo.com>
2020-04-09 14:49:04 +0100

    aaded error tracking

--- handlers/get_projects.go
+++ handlers/get_projects.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/mockingbird-backend/models"
@@ -11,11 +12,14 @@ func handleGetProjects(w http.ResponseWriter, req *http.Request) {
 
 	projects, err := models.ReadProjectsforUser()
 	if err != nil {
+		fmt.Println(err.Error())
 		panic(err)
 	}
 
 	resp, err := json.Marshal(&projects)
 	if err != nil {
+		fmt.Println(err.Error())
+
 		panic(err)
 	}
 
