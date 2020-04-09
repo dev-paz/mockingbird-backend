dc71633ee45b752a710ef77a84291aa47c892e6f
Paz <robertparrish@monzo.com>
2020-04-09 14:55:48 +0100

    aaded error tracking

--- models/projects.go
+++ models/projects.go
@@ -25,12 +25,12 @@ func ReadProjectsforUser() (*[]dto.Project, error) {
 	//
 	var p dto.Project
 	var projects []dto.Project
-	rows, err := db.Query(`SELECT id, name, song, initiated_by, created, status, parts, url, openshot_id FROM projects`)
+	rows, err := db.Query(`SELECT id, name, song, created, status, url, openshot_id FROM projects`)
 	if err != nil {
 		fmt.Println("error querying")
 	}
 	for rows.Next() {
-		err = rows.Scan(&p.ID, &p.Name, &p.Song, &p.Created, &p.Status, &p.OpenshotURL, &p.OpenshotID)
+		err = rows.Scan(&p.ID, &p.Name, &p.Song, &p.Status, &p.OpenshotURL, &p.OpenshotID)
 		if err != nil {
 			// handle this error
 			fmt.Println("error querying by row")
