855b34d311cfc3282124a7cbf5dee8d7ec072dbb
Paz <robertparrish@monzo.com>
2020-04-14 17:00:19 +0100

    adding sign in endpoint

--- models/users.go
+++ models/users.go
@@ -12,7 +12,7 @@ func CreateUser(u *dto.User) error {
 	VALUES ($1, $2, $3, $4, $5)
 	RETURNING id`
 	id := ""
-	err := db.QueryRow(sqlStatement, u.ID, u.Name, u.Email, u.ProfilePhotoURL, u.FirebaseUserID).Scan(&id)
+	err := db.QueryRow(sqlStatement, u.ID, u.FirebaseUserID, u.ProfilePhotoURL, u.Email, u.Name).Scan(&id)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
@@ -24,7 +24,7 @@ func ReadUser(firebaseUserId string) (*dto.User, error) {
 	sqlStatement := `SELECT id, firebase_user_id, profile_photo_url, email, name FROM users WHERE firebase_user_id=$1;`
 	var u dto.User
 	row := db.QueryRow(sqlStatement, firebaseUserId)
-	err := row.Scan(&u.ID, &u.Name, &u.Email, &u.ProfilePhotoURL, &u.FirebaseUserID)
+	err := row.Scan(&u.ID, &u.FirebaseUserID, &u.ProfilePhotoURL, &u.Email, &u.Name)
 	if err != nil {
 		fmt.Printf(err.Error())
 		return nil, err
