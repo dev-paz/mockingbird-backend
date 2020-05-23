4b1b415c207d1380c285f309e18477493516d9fd
Paz <robertparrish@monzo.com>
2020-05-23 16:25:29 +0100

    added backing

--- main.go
+++ main.go
@@ -4,43 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	handler "github.com/mockingbird-backend/handlers"
 	"github.com/mockingbird-backend/models"
 )
 
-// func main() {
-//
-// 	port := os.Getenv("PORT")
-//
-// 	models.InitDB()
-//
-// 	details, err := models.ReadOpenshotDetails()
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		return
-// 	}
-// 	handler.OpenShotIP = details.IP
-//
-// 	handler.HandleRequests()
-// 	log.Fatal(http.ListenAndServe(":"+port, nil))
-// }
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "paz"
-	password = "password"
-	dbname   = "mockingbirdheroku"
-)
-
 func main() {
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	models.InitDB(psqlInfo)
+	port := os.Getenv("PORT")
+
+	models.InitDB()
+
+	details, err := models.ReadOpenshotDetails()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	handler.OpenShotIP = details.IP
 
 	handler.HandleRequests()
-	log.Fatal(http.ListenAndServe(":8881", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
+
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "paz"
+// 	password = "password"
+// 	dbname   = "mockingbirdheroku"
+// )
+//
+// func main() {
+//
+// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+// 		"password=%s dbname=%s sslmode=disable",
+// 		host, port, user, password, dbname)
+// 	models.InitDB(psqlInfo)
+//
+// 	handler.HandleRequests()
+// 	log.Fatal(http.ListenAndServe(":8881", nil))
+// }
